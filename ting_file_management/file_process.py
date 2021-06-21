10d6d90abb71928bac8cf4f1230724639e5eaea7
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-06-21 18:51:46 -0300

    feat: implementation of remove for file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -14,7 +14,10 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return sys.stdout.write("Não há elementos\n")
+    item_removed = instance.dequeue()["nome_do_arquivo"]
+    return sys.stdout.write(f"Arquivo {item_removed} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
