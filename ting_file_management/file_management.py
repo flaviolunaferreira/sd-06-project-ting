c809485b3f407c9c6fa76c44f90fbad1fab49675
mottak <kissylamottapereira@gmail.com>
2021-06-21 16:23:58 -0300

    correcoes req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,12 +3,13 @@ import sys
 
 # https://www.w3schools.com/python/ref_string_splitlines.asp
 def txt_importer(path_file):
-    if path_file:
+    
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato inválido')
+    try:
         lines = ''
-        if not path_file.endswith('.txt'):
-            sys.stderr.write('Formato inválido')
         with open(path_file) as file:
             lines = file.read().splitlines()
         return lines
-    else:
+    except:
         sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
