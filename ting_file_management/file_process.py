e6ecdd11d71cadfaadc322a6030b973d9194d2f7
Tiago Berwanger <berwangertiago@gmail.com>
2021-06-21 13:27:18 -0300

    requirement 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    for item in file:
+        instance.enqueue(item)
+    file_length = instance.__len__()
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": file_length,
+        "linhas_do_arquivo": file
+    }
+    sys.stdout.write(f"{result}\n")
 
 
 def remove(instance):
