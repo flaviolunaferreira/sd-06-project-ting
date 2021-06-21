1771043a530593f34dd7ccc02dde37e9c677f3e6
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-06-21 17:29:29 -0300

    feat: process implementation

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,16 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    text = txt_importer(path_file)
+    output = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text),
+        "linhas_do_arquivo": text
+    }
+    sys.stdout.write(f"{output}\n")
+    return instance.enqueue(output)
 
 
 def remove(instance):
