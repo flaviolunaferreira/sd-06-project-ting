0b42612667aa3b0aca3af42764165e7edfe6b1c8
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-06-21 17:08:29 -0300

    feat: implemente if file is not found

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,4 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    with open(path_file, 'r') as file:
-        text = file.read().splitlines()
-        return text
+    try:
+        if not path_file.endswith('txt'):
+            sys.stderr.write("Formato inválido\n")
+
+        with open(path_file, 'r') as file:
+            text = file.read().splitlines()
+            return text
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
