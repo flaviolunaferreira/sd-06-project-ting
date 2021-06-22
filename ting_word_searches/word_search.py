ba3da5ce4620335d31101cb22d34cf8f0f282483
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-06-22 15:40:54 -0300

    feat: word search implementation

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
+import sys
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    matches = []
+    for index in range(len(instance)):
+        file = instance.search(index)
+        word_found = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for i, line in enumerate(file["linhas_do_arquivo"]):
+            if word in line:
+                word_found["ocorrencias"].append({
+                    "linha": i + 1
+                })
+        if len(word_found["ocorrencias"]):
+            matches.append(word_found)
+    return matches
 
 
 def search_by_word(word, instance):
