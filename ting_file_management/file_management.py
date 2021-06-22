2fd54de2e02fb1541939a409f7b6bb209f2dd928
LeandroParisi <lepcbelisario@gmail.com>
2021-06-22 17:31:29 -0300

    feat: requirement 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,3 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
-    #
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as txt_file:
+            return txt_file.read().splitlines()
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+
+if __name__ == "__main__":
+    txt_importer("tte.txt")
