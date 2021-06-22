03d6edae4dfdbfc2eaa2189f77adc4054419e3ff
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-06-22 19:11:03 -0300

    Req 1 to 5

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if (not os.path.exists(path_file)):
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    extension = path_file.split('.')[-1]
+    if (extension != 'txt'):
+        return print("Formato inválido", file=sys.stderr)
+
+    with open(path_file, 'r') as file:
+        txt = file.read().split('\n')
+    return txt
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,36 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if any(
+        queue["nome_do_arquivo"] == path_file for queue in instance.get_all()
+    ):
+        return "WITH THE BIRDS I'LL SHARE THIS LONELY VIEW"
+
+    lines = txt_importer(path_file)
+    len_lines = len(lines)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len_lines,
+        "linhas_do_arquivo": lines
+    }
+    instance.enqueue(result)
+    return print(result, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if (len(instance.get_all()) == 0):
+        return print("Não há elementos", file=sys.stdout)
+    removed_item = instance.get_all()[0]['nome_do_arquivo']
+    instance.dequeue()
+    return print(
+        f"Arquivo {removed_item} removido com sucesso",
+        file=sys.stdout
+    )
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if (position >= len(instance)):
+        return print("Posição inválida", file=sys.stderr)
+    return print(instance.search(position), file=sys.stdout)
