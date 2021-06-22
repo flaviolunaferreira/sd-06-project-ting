a8f4fd084dc3d5620301ed81cb49be765245fb01
isabellavjs <isabellavjs@gmail.com>
2021-06-22 10:48:02 -0300

    Requirement 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith('.txt'):
+            with open(path_file, mode="r") as txt_file:
+                output = txt_file.readlines()
+                format_output = [line.strip('\n') for line in output]
+                return format_output
+        else:
+            return print("Formato inválido", file=sys.stderr)
+    except OSError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    txt = txt_importer("/home/isabella/sd-06-projects/sd-06-project-ting/statics/arquivo_teste.txt")
