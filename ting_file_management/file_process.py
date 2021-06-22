a8d540b4a63ae6f9ee61311bcdf76a4330eaa2ea
bynho25 <pablo.trybe@gmail.com>
2021-06-21 23:07:19 -0300

    Fixing identation

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -9,12 +9,12 @@ def process(path_file, instance):
         file_read = txt_importer(path_file)
         lines = len(file_read)
         instance.enqueue(path_file)
-        formated_info = {
+        result = {
             "nome_do_arquivo": path_file,
             "qtd_linhas": lines,
             "linhas_do_arquivo": file_read
         }
-    sys.stdout.write(f"{formated_info}\n")
+        sys.stdout.write(f"{result}\n")
 
 
 def remove(instance):
