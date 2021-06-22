4a0ea64ecceb7c8f3a00d2c78673f3a4ac4eabc0
danieleperse <danieleperse3@gmail.com>
2021-06-22 09:12:57 -0300

    req. 7 valida vazio

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -5,7 +5,6 @@ def exists_word(word, instance):
     # [['ola mundo', 'mundo ola'], ['milharal turma', 'turma do milharal']]
     for i in range(file_length):
         parts_file = instance.search(i)
-        print(parts_file)
         occurrence = []
 
         # ['ola mundo', 'mundo ola']
@@ -26,6 +25,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação
-    from ting_file_management.file_management import txt_importer
-    """
+    words = []
+    file_length = instance.__len__()
+
+    # [['ola mundo', 'mundo ola'], ['milharal turma', 'turma do milharal']]
+    for i in range(file_length):
+        parts_file = instance.search(i)
+        occurrence = []
+
+        # ['ola mundo', 'mundo ola']
+        for j in range(len(parts_file)):
+            parts_text = parts_file[j]
+
+            if word in parts_text:
+                occurrence.append({"linha": j + 1, "conteudo": parts_file[j]})
+
+            if len(occurrence) > 0:
+                format_info = {
+                    "palavra": word,
+                    "arquivo": parts_file,
+                    "linhas_do_arquivo": occurrence,
+                }
+                words.append(format_info)
+    return words
