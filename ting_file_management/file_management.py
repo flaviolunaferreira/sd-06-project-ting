a13dd6dd90cd45ff46c7beddcb1e869d138a8bf9
Havyner Caetano <hrcgeo@gmail.com>
2021-06-22 17:44:16 -0300

    feat: requirement 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+
+    try:
+        fileObject = open(path_file, "r")
+    except Exception:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return 0
+    data = fileObject.read()
+    return data.split("\n")
