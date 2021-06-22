836fab9bdc726131692a7f3d2d8599869e9e6e39
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-06-22 18:00:17 -0300

    :sparkles: feat(req 04): def remove on file_process.py

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,11 +1,14 @@
 import sys
 from ting_file_management.file_management import txt_importer
 
+path_list = []
+
 
 def process(path_file, instance):
     if instance.__len__() == 0:
 
         file = txt_importer(path_file)
+        path_list.append(path_file)
 
         file_name = f"'nome_do_arquivo': '{path_file}'"
         lines_qtd = f"'qtd_linhas': {len(file)}"
@@ -21,7 +24,13 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        sys.stdout.write('Não há elementos\n')
+    else:
+        instance.dequeue()
+        path_name = path_list[0]
+        path_list.remove(path_name)
+        sys.stdout.write(f'Arquivo {path_name} removido com sucesso\n')
 
 
 def file_metadata(instance, position):
