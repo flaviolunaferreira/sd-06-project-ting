0c4d7a4dfc4dfeb002e6ce4894f688f9120e5a20
LeandroParisi <lepcbelisario@gmail.com>
2021-06-22 19:33:34 -0300

    feat: requirement 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -19,19 +19,44 @@ def exists_word(word, instance):
                 ocurrences.append(
                     {
                         "linha": index,
+                    }
+                )
+        if len(ocurrences) > 0:
+            search_result["ocorrencias"] = ocurrences
+            output_analysis.append(search_result)
+        else:
+            return []
+
+        sys.stderr.write(str(output_analysis))
+        return output_analysis
+
+
+def search_by_word(word, instance):
+    output_analysis = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        search_result = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+        }
+        for index, line in enumerate(file["linhas_do_arquivo"], 1):
+            ocurrences = []
+            if re.search(word, line, re.IGNORECASE):
+                ocurrences.append(
+                    {
+                        "linha": index,
                         "conteudo": line,
                     }
                 )
         if len(ocurrences) > 0:
             search_result["ocorrencias"] = ocurrences
-            sys.stderr.write(str(search_result))
-            return search_result
+            output_analysis.append(search_result)
         else:
             return []
 
-
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+        sys.stderr.write(str(output_analysis))
+        return output_analysis
 
 
 if __name__ == "__main__":
