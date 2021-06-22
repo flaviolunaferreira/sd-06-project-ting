38fc2543ab2b8d7e4d11d40da9baf1fd8e686b35
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-06-22 15:11:57 -0300

    :sparkles: feat(req 02): file_management.py

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+import os.path
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    is_txt = path_file.endswith('txt')
+    is_path = os.path.isfile(path_file)
+
+    if not is_txt:
+        return sys.stderr.write('Formato inválido\n')
+    elif not is_path:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    else:
+        text = []
+        with open(path_file) as file:
+            for line in file:
+                text.append(line.replace('\n', ''))
+
+        return text
