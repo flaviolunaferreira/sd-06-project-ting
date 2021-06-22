8586d408978d264ccb43d52b01abad433c980aa5
Tiago Berwanger <berwangertiago@gmail.com>
2021-06-21 23:50:16 -0300

    requirement 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -27,4 +27,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        search = instance.search(position)
+        return sys.stdout.write(search)
+    except IndexError:
+        sys.stderr.write('Posição inválida')
