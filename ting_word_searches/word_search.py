a8f637f1cc24efc250e4264b4d2e0bca86a6f7a9
bynho25 <pablo.trybe@gmail.com>
2021-06-21 23:19:54 -0300

    That's all folks

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,24 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
-
+    path_list = instance.return_data()
+    result = list()
+    for file in path_list:
+        content = txt_importer(file)
+        occurrences = list()
+        for index, line in enumerate(content):
+            if word.casefold() in line.casefold():
+                occurrences.append({"linha": index + 1})
+        if len(occurrences) != 0:
+            result_file = {
+                "palavra": word,
+                "arquivo": file,
+                "ocorrencias": occurrences
+            }
+            result.append(result_file)
+    return result
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
