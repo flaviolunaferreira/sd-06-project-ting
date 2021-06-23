92395b776087b2782f89a6d9c40d1129a79cd063
paulo-lins <paulolins100@gmail.com>
2021-06-22 21:49:59 -0300

    2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import os.path
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.isfile(path_file):
+        return sys.stderr.write(
+            f"Arquivo {path_file} não encontrado\n"
+        )
+
+    if path_file.split('.')[1] != "txt":
+        return sys.stderr.write("Formato inválido\n")
+
+    file = open(path_file, mode="r")
+    content = file.read().splitlines()
+    return content
