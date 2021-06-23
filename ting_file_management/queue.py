449dcd90aad9047898d4be262c3468c4d003107a
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-06-23 00:09:35 -0300

    first req

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
+from collections import deque
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.queue.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > len(self.queue) + 1:
+            raise IndexError
+        return self.queue[index]
